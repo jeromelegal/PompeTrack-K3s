37752971c04a40659529fb08bc25345aa54497f5
garth <garth@phylcero.fr>
2026-02-17 20:04:12 +0000

    Fix CLIENT_ID issue on worker

--- apps/worker-fhir/libs/secrets_utils.py
+++ apps/worker-fhir/libs/secrets_utils.py
@@ -1,7 +1,7 @@
 import os
 
 def read_secret_from_file(secret_file_name):
-    pathfile = f"/etc/secrets/{secret_file_name}"
+    pathfile = f"/var/run/secrets/{secret_file_name}"
     if not os.path.exists(pathfile):
         raise FileNotFoundError(f"Secret file not found: {pathfile}")
     with open(pathfile, "r") as f:
