de77c7944f7d9aa4ffb898dc343f1f5f91023007
garth <garth@phylcero.fr>
2026-02-17 14:46:19 +0000

    Update symptoms list

--- apps/worker-fhir/fhir_codes/symptoms_code.py
+++ apps/worker-fhir/fhir_codes/symptoms_code.py
@@ -1,224 +1,237 @@
 SYMPTOMS_CODE = {
-  "acné": {
+  "Acné": {
     "system": "http://snomed.info/sct",
     "code": "88616000",
     "display": "Acne vulgaris",
     "text": "Acné"
   },
-  "ballonnements": {
+  "Ballonnements": {
     "system": "http://snomed.info/sct",
     "code": "116289008",
     "display": "Abdominal bloating",
     "text": "Ballonnements"
   },
-  "bouffées de chaleur": {
+  "Bouffées de chaleur": {
     "system": "https://app.phylcero.fr/codes",
     "code": "hotflushes",
     "display": "Hot flushes",
     "text": "Bouffées de chaleur"
   },
-  "brulures d'estomac": {
+  "Brulures d'estomac": {
     "system": "http://snomed.info/sct",
     "code": "16331000",
     "display": "Heartburn",
     "text": "Brulures d'estomac"
   },
-  "changements de l'appétit": {
+  "Changements de l'appétit": {
     "system": "https://app.phylcero.fr/codes",
     "code": "appetitechanges",
     "display": "Appetite changes",
     "text": "Changements de l'appétit"
   },
-  "changements de l'humeur": {
+  "Changements de l'humeur": {
     "system": "https://app.phylcero.fr/codes",
     "code": "moodchanges",
     "display": "Mood changes",
     "text": "Changements de l'humeur"
   },
-  "changements du sommeil": {
+  "Changements du sommeil": {
     "system": "https://app.phylcero.fr/codes",
     "code": "sleepchanges",
     "display": "Sleep changes",
     "text": "Changements du sommeil"
   },
-  "chute de cheveux": {
+  "Chute de cheveux": {
     "system": "https://app.phylcero.fr/codes",
     "code": "hairloss",
     "display": "Hair loss",
     "text": "Chute de cheveux"
   },
-  "congestion": {
+  "Congestion": {
     "system": "https://app.phylcero.fr/codes",
     "code": "congestion",
     "display": "Congestion",
     "text": "Congestion"
   },
-  "constipation": {
+  "Constipation": {
     "system": "https://app.phylcero.fr/codes",
     "code": "constipation",
     "display": "Constipation",
     "text": "Constipation"
   },
-  "courbatures et douleurs musculaires": {
+  "Courbatures et douleurs musculaires": {
     "system": "https://app.phylcero.fr/codes",
     "code": "muscleachesandpain",
     "display": "Muscle aches and pain",
     "text": "Courbatures et douleurs musculaires"
   },
-  "crampes abdominales": {
+  "Crampes abdominales": {
     "system": "https://app.phylcero.fr/codes",
     "code": "abdominalcramps",
     "display": "Abdominal cramps",
     "text": "Crampes abdominales"
   },
-  "diarrhée": {
+  "Diarrhée": {
     "system": "http://snomed.info/sct",
     "code": "62315008",
     "display": "Diarrhea",
     "text": "Diarrhée"
   },
-  "douleurs pelviennes": {
+  "Douleurs pelviennes": {
     "system": "https://app.phylcero.fr/codes",
     "code": "pelvicpain",
     "display": "Pelvic pain",
     "text": "Douleurs pelviennes"
   },
-  "écoulement nasal": {
+  "Écoulement nasal": {
     "system": "https://app.phylcero.fr/codes",
     "code": "nasaldischarge",
     "display": "Nasal discharge",
     "text": "Écoulement nasal"
   },
-  "essoufflement": {
+  "Essoufflement": {
     "system": "https://app.phylcero.fr/codes",
     "code": "shortnessofbreath",
     "display": "Shortness of breath",
     "text": "Essoufflement"
   },
-  "évanouissement": {
+  "Évanouissement": {
     "system": "https://app.phylcero.fr/codes",
     "code": "fainting",
     "display": "Fainting",
     "text": "Évanouissement"
   },
-  "extrasystole": {
+  "Extrasystole": {
     "system": "https://app.phylcero.fr/codes",
     "code": "extrasystole",
     "display": "Extrasystole",
     "text": "Extrasystole"
   },
-  "fièvre": {
+  "Fièvre": {
     "system": "http://snomed.info/sct",
     "code": "386661006",
     "display": "Fever",
     "text": "Fièvre"
   },
-  "frissons": {
+  "Frissons": {
     "system": "http://snomed.info/sct",
     "code": "43724002",
     "display": "Chill",
     "text": "Frissons"
   },
-  "incontinence urinaire": {
+  "Incontinence urinaire": {
     "system": "https://app.phylcero.fr/codes",
     "code": "urinaryincontinence",
     "display": "Urinary incontinence",
     "text": "Incontinence urinaire"
   },
-  "mastodynie": {
+  "Mastodynie": {
     "system": "https://app.phylcero.fr/codes",
     "code": "breastpain",
     "display": "Breast pain",
     "text": "Mastodynie"
   },
-  "maux de gorge": {
+  "Maux de gorge": {
     "system": "http://snomed.info/sct",
     "code": "162397003",
     "display": "Pain in throat",
     "text": "Maux de gorge"
   },
-  "maux de tête": {
+  "Maux de tête": {
     "system": "http://snomed.info/sct",
     "code": "25064002",
     "display": "Headache",
     "text": "Maux de tête"
   },
-  "nausées": {
+  "Nausées": {
     "system": "http://snomed.info/sct",
     "code": "422587007",
     "display": "Nausea",
     "text": "Nausées"
   },
-  "oppression ou douleur thoracique": {
+  "Oppression ou douleur thoracique": {
     "system": "https://app.phylcero.fr/codes",
     "code": "chesttightnessorpain",
     "display": "Chest tightness or pain",
     "text": "Oppression ou douleur thoracique"
   },
-  "perte de l'odorat": {
+  "Perte de l'odorat": {
     "system": "https://app.phylcero.fr/codes",
     "code": "lossofsmell",
     "display": "Loss of smell",
     "text": "Perte de l'odorat"
   },
-  "perte du goût": {
+  "Perte du goût": {
     "system": "https://app.phylcero.fr/codes",
     "code": "lossoftaste",
     "display": "Loss of taste",
     "text": "Perte du goût"
   },
-  "respiration sifflante": {
+  "Respiration sifflante": {
     "system": "https://app.phylcero.fr/codes",
     "code": "wheezing",
     "display": "Wheezing",
     "text": "Respiration sifflante"
   },
-  "rythme cardiaque rapide, martèlement ou palpitations": {
+  "Rythme cardiaque rapide, martèlement ou palpitations": {
     "system": "https://app.phylcero.fr/codes",
     "code": "rapidpoundingorpalpitations",
     "display": "Rapid, pounding or palpitations",
     "text": "Rythme cardiaque rapide, martèlement ou palpitations"
   },
-  "sècheresse cutanée": {
+  "Sècheresse cutanée": {
     "system": "https://app.phylcero.fr/codes",
     "code": "dryskin",
     "display": "Dry skin",
     "text": "Sècheresse cutanée"
   },
-  "sècheresse vaginale": {
+  "Sècheresse vaginale": {
     "system": "https://app.phylcero.fr/codes",
     "code": "vaginaldryness",
     "display": "Vaginal dryness",
     "text": "Sècheresse vaginale"
   },
-  "sueurs nocturnes": {
+  "Sueurs nocturnes": {
     "system": "https://app.phylcero.fr/codes",
     "code": "nightsweats",
     "display": "Night sweats",
     "text": "Sueurs nocturnes"
   },
-  "toux": {
+  "Toux": {
     "system": "http://snomed.info/sct",
     "code": "49727002",
     "display": "Cough",
     "text": "Toux"
   },
-  "trous de mémoire": {
+  "Trous de mémoire": {
     "system": "https://app.phylcero.fr/codes",
     "code": "memorylapses",
     "display": "Memory lapses",
     "text": "Trous de mémoire"
   },
-  "vertiges": {
+  "Vertiges": {
     "system": "http://snomed.info/sct",
     "code": "399153001",
     "display": "Vertigo",
     "text": "Vertiges"
   },
-  "vomissements": {
+  "Vomissements": {
     "system": "http://snomed.info/sct",
     "code": "422400008",
     "display": "Vomiting",
     "text": "Vomissements"
+  },{
+  "Fatigue": {
+    "system": "http://snomed.info/sct",
+    "code": "84229001",
+    "display": "Fatigue (finding)",
+    "text": "Fatigue"
+  },
+  "Lombalgie": {
+    "system": "http://snomed.info/sct",
+    "code": "279039007",
+    "display": "Low back pain (finding)",
+    "text": "Lombalgie"
   }
+}
 }
