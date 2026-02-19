13f10728e8b1b8ecc1c5a3a1554ff1b576b28f51
garth <garth@phylcero.fr>
2026-02-19 10:19:49 +0000

    Fix clients secrets

--- apps/ingestion/entrypoint.sh
+++ apps/ingestion/entrypoint.sh
@@ -5,5 +5,11 @@ fi
 if [ -n "$MINIO_SECRET_ACCESS_KEY_FILE" ] && [ -f "$MINIO_SECRET_ACCESS_KEY_FILE" ]; then
   export MINIO_SECRET_ACCESS_KEY="$(tr -d '\r\n' < "$MINIO_SECRET_ACCESS_KEY_FILE")"
 fi
+if [ -n "$CLIENT_ID_FILE" ] && [ -f "$CLIENT_ID_FILE" ]; then
+  export CLIENT_ID="$(tr -d '\r\n' < "$CLIENT_ID_FILE")"
+fi
+if [ -n "$CLIENT_SECRET_FILE" ] && [ -f "$CLIENT_SECRET_FILE" ]; then
+  export CLIENT_SECRET="$(tr -d '\r\n' < "$CLIENT_SECRET_FILE")"
+fi
 
 exec "$@"
