33899f4eed154a70679e5ba0c8c74c194353867b
garth <garth@phylcero.fr>
2026-02-19 10:19:49 +0000

    Fix clients secrets

--- apps/worker-sqlite/entrypoint.sh
+++ apps/worker-sqlite/entrypoint.sh
@@ -2,5 +2,11 @@
 if [ -n "$TOKEN_FILE" ] && [ -f "$TOKEN_FILE" ]; then
   export TOKEN="$(tr -d '\r\n' < "$TOKEN_FILE")"
 fi
+if [ -n "$CLIENT_ID_FILE" ] && [ -f "$CLIENT_ID_FILE" ]; then
+  export CLIENT_ID="$(tr -d '\r\n' < "$CLIENT_ID_FILE")"
+fi
+if [ -n "$CLIENT_SECRET_FILE" ] && [ -f "$CLIENT_SECRET_FILE" ]; then
+  export CLIENT_SECRET="$(tr -d '\r\n' < "$CLIENT_SECRET_FILE")"
+fi
 
 exec "$@"
